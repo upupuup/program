d56ec7c642aca3c2ff117a1b364932d3c539d713
jiangzhihong <woyaojiayou_a@163.com>
2020-05-25 19:31:19 +0800

    sql

---
+++ src/main/java/com/navi/mini/program/sql.sql
@@ -0,0 +1,14 @@
+-- alter table ret_wo add INSP_TIMESTAMP timestamp(0) null comment "质检申请时间";inWharfChangerTimestamp
+alter table ret_wo add IN_WHARF_CHANGER_TIMESTAMP timestamp(0) null comment "更换码头申请时间";
+alter table his_ret_wo add IN_WHARF_CHANGER_TIMESTAMP timestamp(0) null comment "更换码头申请时间";
+
+alter table his_ret_wo add APPROVAL_STATUS char(1) null comment "C待审批|A审批中|O审批结束";
+alter table his_ret_wo add APPROVAL_RESULTS char(1) null comment "Y:同意|N:拒绝";
+alter table his_ret_wo add APPROVAL_COMMENTS text(0) null comment "审批意见";
+
+alter table his_ret_wo add ADD_TYPE int(1) not null comment "添加的类型：0：正常添加，1：码头更换添加，2：质检添加";
+
+alter table ret_box_emp_no add BOX_NUM int(5) not null comment "箱数";
+alter table ret_box_emp_no add PALLET_NUM int(5) not null comment "托数";
+alter table ret_box_emp_no add APPLY_TIME timestamp(5)  null comment "申请时间";
+alter table ret_box_emp_no add APPROVE_TIME timestamp(5) null comment "审核时间";
